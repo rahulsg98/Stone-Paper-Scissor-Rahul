3a14bd735f0e4374a3f97bd2d2baf5cc528756d5
Rahul Singh Gurjar <78016968+rahulsg98@users.noreply.github.com>
2023-03-13 00:12:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const ruleList = document.getElementById('rules-list');
 const crossIcon = document.getElementById('icon');
 const ruleBtn = document.getElementById('btn');
+const nextBtn = document.getElementById('next-button');
+
+const shade1 = document.getElementById('shade1');
+const shade2 = document.getElementById('shade2');
+
+shade1.style.visibility = 'hidden';
+shade2.style.visibility = 'hidden';
+
+const celebrateSection = document.getElementById('celebrate-section');
+const starcupSection = document.getElementById('star-cup');
+celebrateSection.style.visibility = 'hidden';
+// starcupSection.style.visibility = 'hidden';
+
+nextBtn.style.visibility = 'hidden';
 
 ruleList.style.visibility = 'hidden';
 crossIcon.style.visibility = 'hidden';
@@ -8,8 +22,19 @@ crossIcon.style.visibility = 'hidden';
 var userScore = document.getElementById('user-score-value');
 var compScore = document.getElementById('comp-score-value');
 
-var userCount = 0;
-var compCount = 0;
+
+var userCount = localStorage.getItem("userCount");
+var compCount = localStorage.getItem("compCount");
+
+if (userCount == null) {
+    userScore.innerText = 0;
+    compScore.innerText = 0;
+}
+else{
+    userScore.innerText = userCount;
+    compScore.innerText = compCount;
+}
+
 let cntbtn=0;
 
 ruleBtn.addEventListener("click", function() {
@@ -29,6 +54,8 @@ crossIcon.addEventListener("click", function() {
     crossIcon.style.visibility = 'hidden';
     cntbtn=0;
 });
+
+const template = document.getElementById('template');
 
 const stone = document.getElementById('stone');
 const paper = document.getElementById('paper');
@@ -60,29 +87,68 @@ var playAgain = document.getElementById('play-again');
 playAgain.addEventListener("click", function(){
     gameSection.style.visibility = 'visible';
     resultSection.style.visibility = 'hidden';
+
+    nextBtn.style.visibility = 'hidden';
+
+    ruleBtn.style.left = "1210px";
+    shade1.style.visibility = 'hidden';
+    shade2.style.visibility = 'hidden';
 })
 
 function Won(){
     statusText.innerText = "YOU WON";
     playAgain.innerText = "PLAY AGAIN";
-    userCount++;
+    // userCount++;
+
+    //userCount = localStorage.getItem("userCount");
+    userCount = userCount ? parseInt(userCount) + 1 : 1;
+    localStorage.setItem("userCount", userCount);
+
     userScore.innerText = userCount;
+
+    nextBtn.style.visibility = 'visible';
+
+    ruleBtn.style.left = "1080px";
+    nextBtn.style.left = "1210px";
+
+    shade1.style.visibility = 'visible';
+    
 }
 
 function Lost(){
     statusText.innerText = "YOU LOST";
     playAgain.innerText = "PLAY AGAIN";
-    compCount++;
+    // compCount++;
+
+    //compCount = localStorage.getItem("compCount");
+    compCount = compCount ? parseInt(compCount) + 1 : 1;
+    localStorage.setItem("compCount", compCount);
+
     compScore.innerText = compCount;
+
+    ruleBtn.style.left = "1210px";
+
+    shade2.style.visibility = 'visible';
 }
 
 function Tie(){
     statusText.innerText = "TIE UP";
     playAgain.innerText = "REPLAY";
-    compCount++;
-    userCount++;
+    // compCount++;
+    // userCount++;
+
+    //userCount = localStorage.getItem("userCount");
+    userCount = userCount ? parseInt(userCount) + 1 : 1;
+    localStorage.setItem("userCount", userCount);
+
+    //compCount = localStorage.getItem("compCount");
+    compCount = compCount ? parseInt(compCount) + 1 : 1;
+    localStorage.setItem("compCount", compCount);
+
     compScore.innerText = compCount;
     userScore.innerText = userCount;
+
+    ruleBtn.style.left = "1210px";
 }
 
 stone.addEventListener("click",function(){
@@ -91,7 +157,7 @@ stone.addEventListener("click",function(){
 
     userChoice.style.border = "16px solid #0074B6";
     userChoice.style.filter = "drop-shadow(-1px 1px 11px rgba(0, 0, 0, 0.35))";
-    ruleArea.style.marginTop = "-260px";
+    ruleArea.style.marginTop = "-450px";
 
     var randomNumber = Math.floor(Math.random()*3);
 
@@ -116,7 +182,7 @@ paper.addEventListener("click",function(){
 
     userChoice.style.border = "16px solid #FFA943";
     userChoice.style.filter = "drop-shadow(-1px 1px 11px rgba(0, 0, 0, 0.35))";
-    ruleArea.style.marginTop = "-260px";
+    // ruleArea.style.marginTop = "-260px";
 
     var randomNumber = Math.floor(Math.random()*3);
 
@@ -158,4 +224,26 @@ scissor.addEventListener("click",function(){
     else if(choicesArr[randomNumber] === 'scissor'){
         Tie();
     }
+})
+
+nextBtn.addEventListener("click", function(){
+    template.style.visibility = 'hidden';
+    resultSection.style.visibility = 'hidden';
+    celebrateSection.style.visibility = 'visible';
+    nextBtn.style.visibility = 'hidden';
+
+    ruleBtn.style.left = "1210px";
+
+    shade1.style.visibility = 'hidden';
+    shade2.style.visibility = 'hidden';
+})
+
+const celePlayAgain = document.getElementById('cele-play-again');
+
+celePlayAgain.addEventListener("click", function(){
+    template.style.visibility = 'visible';
+    gameSection.style.visibility = 'visible';
+    celebrateSection.style.visibility = 'hidden';
+
+    ruleBtn.style.left = "1210px";
 })
